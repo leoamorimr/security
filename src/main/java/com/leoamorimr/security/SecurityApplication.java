9b361d64b0c7154d4b848fa1e4210d0a60ba8387
leoamorimr <leoamorimr@gmail.com>
2022-01-04 17:06:21 +0000

    Including persistence to main class.

--- src/main/java/com/leoamorimr/security/SecurityApplication.java
+++ src/main/java/com/leoamorimr/security/SecurityApplication.java
@@ -1,13 +1,35 @@
 package com.leoamorimr.security;
 
+import com.leoamorimr.security.domain.Role;
+import com.leoamorimr.security.domain.User;
+import com.leoamorimr.security.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
 
+    @Bean
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "Leonardo", "leoamorimr", "123", new ArrayList<>()));
+            userService.saveUser(new User(null, "Thayse", "thaygc", "123", new ArrayList<>()));
+
+            userService.addRoleToUser("leoamorimr", "ROLE_ADMIN");
+            userService.addRoleToUser("thaygc", "ROLE_MANAGER");
+        };
+    }
 }
