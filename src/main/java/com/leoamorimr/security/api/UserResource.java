db8c070f88f821380416a60755014376a0dd9fce
leoamorimr <leoamorimr@gmail.com>
2022-01-04 16:16:58 +0000

    Implementation of UserResource

--- src/main/java/com/leoamorimr/security/api/UserResource.java
+++ src/main/java/com/leoamorimr/security/api/UserResource.java
@@ -1,15 +1,21 @@
 package com.leoamorimr.security.api;
 
+import java.net.URI;
 import java.util.List;
 
+import com.leoamorimr.security.domain.Role;
 import com.leoamorimr.security.domain.User;
 import com.leoamorimr.security.service.UserService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,4 +28,28 @@ public class UserResource {
   public ResponseEntity<List<User>> getUsers() {
     return ResponseEntity.ok().body(userService.getUsers());
   }
+
+  @PostMapping("/user/save")
+  public ResponseEntity<User> saveUser(@RequestBody User user) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveUser(user));
+  }
+
+  @PostMapping("/role/save")
+  public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+    return ResponseEntity.created(uri).body(userService.saveRole(role));
+  }
+
+  @PostMapping("/role/addToUser")
+  public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+    userService.addRoleToUser(form.getRolename(), form.getRolename());
+    return ResponseEntity.ok().build();
+  }
+}
+
+@Data
+class RoleToUserForm {
+  String username;
+  String rolename;
 }
